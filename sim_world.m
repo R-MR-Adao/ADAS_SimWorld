61553f778f3dd2e60d4e5fa00e168b1e580d0cb6
R-MR-Adao <rm.ribeiro.adao@gmail.com>
2022-10-05 15:59:29 +0100

    Add standing objects

--- sim_world.m
+++ sim_world.m
@@ -32,69 +32,98 @@ function sim_world()
     road.T = 40;            % (m) road periodicity
     road.x = 0:0.2:80;      % (m) road x array
     road.y = @(x) ...       % (m) road y array
-        road.T/2*sin(2*pi/road.T*x).*cos(2*pi/road.T/3*x);
+        road.T/2*sin(2*pi/road.T*x).*cos(2*pi/road.T/2*x);
     
     ego = [];               % ego properties
-    ego.v = 5;             % (m/s) ego speed
+    ego.v = 5;              % (m/s) ego speed
     ego.x = @(t,x_1)...     % (m) ego x position
         ego.v*t - road.x(end)*floor(x_1/road.x(end));
     ego.y = @(x) road.y(x); % (m) ego y position
     ego.x_1 = 0;            % (m) ego's last x
     
+    stand_n_objects = 10;
+    stand_rg_x = [min(road.x) max(road.x)]; % range for standing objects
+    stand_rg_y = [-1 1]*road.T ; % range for standing objects
+    stand = []; % standing (static) objects properties
+    % randomly generate standing object positions
+    stand.x = rand(stand_n_objects,1)*diff(stand_rg_x) + stand_rg_x(1);
+    stand.y = rand(stand_n_objects,1)*diff(stand_rg_y) + stand_rg_y(1);
+    
     % ************ initialize simulation animation variables *************
     t = 0;                                      % (s) time
-    set(interface.main_figure.ax_static,...     % dynamic axes limits
+    set(interface.main_figure.ax_static,... % dynamic axes limits
         'xlim', [-20 20],...
         'ylim', [0 80]);
-    set(interface.main_figure.ax_dynamic,...    % dynamic axes limits
+    set(interface.main_figure.ax_dynamic,...% dynamic axes limits
         'xlim', [-20 20],...
         'ylim', [-40 40]);
-    road_tail = road.x(round(end/2));           % (m) road tail behind ego
+    road_tail = road.x(round(end/2));       % (m) road tail behind ego
     
-    road_x = road.x;
-    road_y = road.y(road_x);
+    % initialze common variables
+    road_x = road.x;            % road x array
+    road_y = road.y(road_x);    % road y array
     
-    ego_x = ego.x(t,ego.x_1);
-    ego_y = ego.y(ego_x);
+    ego_x = ego.x(t,ego.x_1);   % ego x position
+    ego_y = ego.y(ego_x);       % ego y position
     
-    % static axis
+    % static axes plots
     road.m.static = plot(interface.main_figure.ax_static,...
         road_y, road_x,'w','linewidth',2);
     ego.m.static =  plot(interface.main_figure.ax_static,...
         ego_y,ego_x,'or','linewidth',2);
+    stand(1).m.static = plot(interface.main_figure.ax_static,...
+        stand(1).y, stand(1).x,'og','linewidth',2);
     
-    % dynamic axes
-    [road_x,road_y] = dynamic_transform_coordinates(...
+    % dynamic axes transformations
+    % transform road coordinates
+    [road_x,road_y,theta] = dynamic_transform_coordinates(...
         road_x,road_y,ego_x,ego_y);
+    % transform standing object coordinates
+    [s_x,s_y] = dynamic_transform_coordinates(...
+            stand.x,stand.y,ego_x,ego_y, theta);
+    
+    % dynamic axes plots
     road.m.dynamic = plot(interface.main_figure.ax_dynamic,...
         road_y,road_x,'w','linewidth',2);
     ego.m.dynamic =  plot(interface.main_figure.ax_dynamic,...
         0,0,'or','linewidth',2);
+    for ii = 1 : stand_n_objects
+        stand.m(ii).dynamic = plot(interface.main_figure.ax_dynamic,...
+            s_y(ii),s_x(ii),'og','linewidth',2);
+    end
     
     % ************************* simulation start *************************
     
     while t < 100;
         
-        % define plot properties
-        
         % update common variables
-        ego_x = ego.x(t,ego.x_1);
-        ego_y = ego.y(ego_x);
+        ego_x =  ego.x(t,ego.x_1);
+        ego_y =  ego.y(ego_x);
         road_x = road.x + ego.x_1-road_tail;
         road_y = road.y(road_x);
         
         % ************************* static axes *************************
         
-        % update ego
+        % update static axes
         set(ego.m.static,'xData',ego_y,'yData',ego_x)
         ego.x_1 = ego.x(t,0);
         
         % ************************* dynamic axes *************************
         
-        % update road
-        [road_x,road_y] = dynamic_transform_coordinates(...
-        road_x,road_y,ego.x_1,ego.y(ego.x_1));
+        % update dynamic varibles
+        [road_x,road_y,theta] = dynamic_transform_coordinates(...
+            road_x,road_y,ego.x_1,ego.y(ego.x_1));
+        % shift standing objects for smoother rendering
+        s_shift = road.x(end)*((ego_x-stand.x) > road.x(round(end/2))) +... 
+                 -road.x(end)*((stand.x-ego_x) > road.x(round(end/2)));
+        [s_x,s_y] = dynamic_transform_coordinates(...
+            stand.x+s_shift,stand.y,ego_x,ego_y, theta);
+        
+        % update dynamic axes
         set(road.m.dynamic,'xData',road_y,'yData',road_x)
+        for ii = 1 : stand_n_objects
+            set(stand.m(ii).dynamic,'xData',s_y(ii),'ydata',s_x(ii));
+        end
         
         % increment time
         t = t + dt;
@@ -103,7 +132,8 @@ function sim_world()
     
     % *********************** function definitions ***********************
     
-    function [x, y] = dynamic_transform_coordinates(x,y,x_ref,y_ref)
+    function [x,y,theta] = dynamic_transform_coordinates(...
+            x,y,x_ref,y_ref,theta)
         % rotation matrix
         r = @(x,y,t) [x(:),y(:)]*[cos(t) -sin(t);...
                                   sin(t)  cos(t)];
@@ -111,13 +141,15 @@ function sim_world()
         x = x - x_ref;
         y = y - y_ref;
         
-        %find root
-        [~,i_root] = min(abs(x));
-                
-        % calculate rotation angle
-        dx = x(i_root+1) - x(i_root);
-        dy = y(i_root+1) - y(i_root);
-        theta = atan2(dy,dx);
+        if nargin  < 5
+            %find root
+            [~,i_root] = min(abs(x));
+
+            % calculate rotation angle
+            dx = x(i_root+1) - x(i_root);
+            dy = y(i_root+1) - y(i_root);
+            theta = atan2(dy,dx);
+        end
         
         % apply rotation
         xy = r(x,y,theta);
