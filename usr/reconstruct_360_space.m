df87dc9253e75b101f6eb3c7fc9da384c80cb805
R-MR-Adao <rm.ribeiro.adao@gmail.com>
2022-10-09 12:24:40 +0100

    Add user code textbox

---
+++ usr/reconstruct_360_space.m
@@ -0,0 +1,9 @@
+function [stand,mov,onc] = reconstruct_360_space(sensor)
+    % Reconstruct 360 EM space around the ego from the sensor measurements
+    % @param    sensor An n-sensors-long array containing the standing,
+    %           moving, and oncoming object detections in each sensor's FoV
+    % @returns stand,mov,onc [x(:) y(:)]shaped arrays containing the
+    %          standing, moving and oncoming detections in the
+    %          reconstructed space
+    
+end
